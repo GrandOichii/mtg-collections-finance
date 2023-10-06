ab6ef06ddcfc7ad1ba48bc6142023c598c9026de
Igor Yastrebov <i.hawk@yandex.ru>
2023-10-06 12:59:25 +0400

    Added double-faced cards to collecvtion cards

--- CollectionCard.cs
+++ CollectionCard.cs
@@ -8,14 +8,13 @@ public partial class CollectionCard : Control
 {
 	#region Nodes
 	
-	public TextureRect ImageTextureNode { get; private set; } 
 	public Label NameLabelNode { get; private set; }
 	public SpinBox AmountSpinNode { get; private set; }
-	public HttpRequest ImageRequestNode { get; private set; }
 	public ColorRect TransparencyRectNode { get; private set; }
 	public RichTextLabel PriceLabelNode { get; private set; }
 	public Button PrintingButtonNode { get; private set; }
-	
+	public MTGCardBase CardNode { get; private set; }
+
 	#endregion
 
 	#region Signals
@@ -28,7 +27,6 @@ public partial class CollectionCard : Control
 	public CCard Data { get; private set; }
 	public ShortCard CData { get; private set; }
 	
-	private Texture2D _defaultTex;
 	private string _priceType;
 	
 	public List<Card> Variations { get; private set; }
@@ -46,16 +44,13 @@ public partial class CollectionCard : Control
 		#region Node fetching
 		
 		NameLabelNode = GetNode<Label>("%NameLabel");
-		ImageTextureNode = GetNode<TextureRect>("%ImageTexture");
 		AmountSpinNode = GetNode<SpinBox>("%AmountSpin");
-		ImageRequestNode = GetNode<HttpRequest>("%ImageRequest");
 		TransparencyRectNode = GetNode<ColorRect>("%TransparencyRect");
 		PriceLabelNode = GetNode<RichTextLabel>("%PriceLabel");
 		PrintingButtonNode = GetNode<Button>("%PrintingButton");
-		
-		#endregion
-		
-		_defaultTex = ImageTextureNode.Texture;
+		CardNode = GetNode<MTGCardBase>("%Card");
+
+		#endregion		
 	}
 	
 	private void UpdateVariantData() {
@@ -64,12 +59,11 @@ public partial class CollectionCard : Control
 		PrintingButtonNode.Text = _variant.UID;
 		Data.Printing = _variant.ID;
 
-		ImageRequestNode.Request(_variant.ImageURIs["normal"]);
+		CardNode.Load(Variant);
 		UpdatePrice();
 	}
 	
 	public void Load(CCard cCard, Wrapper<ShortCard>? cardW, string priceType) {
-		ImageTextureNode.Texture = _defaultTex;
 		_priceType = priceType;
 		Data = cCard;
 		CData = cardW.Value;
@@ -122,26 +116,7 @@ public partial class CollectionCard : Control
 	// TODO check for null
 	public double TotalPrice => (_variant.Prices[_priceType] is not null ? Math.Round(double.Parse(_variant.Prices[_priceType], CultureInfo.InvariantCulture) * AmountSpinNode.Value, 2) : 0);
 	
-	private void SetTexture(Texture2D tex) {
-		ImageTextureNode.Texture = tex;
-
-	}
-
 	#region Signal connections
-	
-	private void _on_image_request_request_completed(long result, long response_code, string[] headers, byte[] body)
-	{
-		if (response_code != 200) {
-			// 
-			return;
-		}
-		Task.Run(() => {
-			var image = new Image();
-			image.LoadJpgFromBuffer(body);
-			var tex = ImageTexture.CreateFromImage(image);
-			CallDeferred("SetTexture", tex);
-		});
-	}
 	
 	private void _on_amount_spin_value_changed(double v)
 	{
